a3b5cea2210b1710478431ab7cc675d3a355c198
Your Name <you@example.com>
2025-03-23 01:06:22 +0400

    update 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import data from "./data.js";
-import { FaInstagram, } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 import { AiFillTikTok } from "react-icons/ai";
+import { SiTelegram } from "react-icons/si";
 // import { TfiEmail } from "react-icons/tfi";
 import "./css/App.css";
 import TicketCard from "./components/TicketCard/TicketCard";
@@ -10,28 +11,34 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="ticket_head">
-          <img className="logo" src="/img/photo_2025-03-17_20-36-31.jpg" />
+          <img className="logo" src="/img/Logo.png" />
           <h2 className="subtitle">YVN PARTY</h2>
           <p className="direction">JUST FOR FUN</p>
           <p className="direction">հարցերի դեպքում +374(33) 180 033</p>
-          
-          
+
           <div className="incon_box">
-            <a href="https://www.instagram.com/yvnparty/" target="_blank"><FaInstagram size={35}/></a>
-            <a href="https://www.tiktok.com/@yvnparty?_t=ZS-8uiGTXNsmgx&_r=1" target="_blank"><AiFillTikTok size={35} /></a>
-            {/* <TfiEmail size={35}/> */}
+            <a href="https://www.instagram.com/yvnparty/" target="_blank">
+              <FaInstagram size={35} />
+            </a>
+            <a
+              href="https://www.tiktok.com/@yvnparty?_t=ZS-8uiGTXNsmgx&_r=1"
+              target="_blank"
+            >
+              <AiFillTikTok size={35} />
+            </a>
+            <a href="https://t.me/yvnparty2024" target="_blank">
+              <SiTelegram size={35} />
+            </a>
           </div>
         </div>
         <div className="ticket_cards">
-            {
-              data.map((card) => {
-                return <TicketCard key={card.id} card={card} />
-              })
-            }
+          {data.map((card) => {
+            return <TicketCard key={card.id} card={card} />;
+          })}
         </div>
       </div>
     </div>
-  );  
+  );
 }
 
 export default App;
