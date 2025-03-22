eb7ff8ce8cd8b99d47308e05deae1353ff9e0692
Your Name <you@example.com>
2025-03-23 00:30:22 +0400

    update

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 const data = [
     {
         id: 1,
-        url: "src/img/photo_2025-03-17_21-26-13.jpg",
+        url: "public/img/photo_2025-03-17_21-26-13.jpg",
         text: "Գնել տոմսը  Fast Ticket-ից",
         link: "https://www.fastticket.am/zhamancayin/e/cernyi-bvoumer"
     },
     {
         id: 2,
-        url: "src/img/cardPhoto2.webp",
+        url: "public/img/cardPhoto2.webp",
         text: "Գնել տոմսը TicketON Wallet-ից",
         link: "https://ticketon.am/hy/event/3495?fbclid=PAZXh0bgNhZW0CMTEAAaZU0rmEEQdpXbRc_0gGX_h0079HNV-w15wQCVfEvQNGT_RmyrIHX93KxUs_aem_sW2OhF_S5fyjJj3fAIN0Bw",
     },
     {
         id: 3,
-        url: "src/img/cardPhoto3.webp",
+        url: "public/img/cardPhoto3.webp",
         text: "Գնել տոմսը Tomsarkgh.am-ից",
         link: "https://www.tomsarkgh.am/hy/event/48849/"
     },
