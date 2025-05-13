3a419b21095528ec2a3200c86c2be2ed045bc734
Your Name <you@example.com>
2025-05-13 17:06:41 +0400

    ByTicket project --v 2

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import data from "./data.js";
+import { FaInstagram } from "react-icons/fa";
+import { AiFillTikTok } from "react-icons/ai";
+import { SiTelegram } from "react-icons/si";
+import "./css/App.css";
+import TicketCard from "./components/TicketCard/TicketCard";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="ticket_head">
+          <img className="logo" src="/img/logo_2.png" />
+          <h2 className="subtitle"><span>YVN</span> PARTY</h2>
+          <p className="direction">JUST FOR FUN</p>
+          <p className="direction">հարցերի դեպքում +374(33) 180 033</p>
+
+          <div className="incon_box">
+            <a href="https://www.instagram.com/yvnparty/" target="_blank">
+              <FaInstagram size={35} />
+            </a>
+            <a
+              href="https://www.tiktok.com/@yvnparty?_t=ZS-8uiGTXNsmgx&_r=1"
+              target="_blank"
+            >
+              <AiFillTikTok size={35} />
+            </a>
+            <a href="https://t.me/yvnparty2024" target="_blank">
+              <SiTelegram size={35} />
+            </a>
+          </div>
+        </div>
+        <div className="ticket_cards">
+          {data.map((card) => {
+            return <TicketCard key={card.id} card={card} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: 1,
+        url: "/PNG/Blue.png",
+        text: "Գնել տոմսը  Event hub-ից",
+        link: "https://eventhub.am/am/club/event/california-night-1c4f5d9c-92eb-45e8-8fe3-13ee5ac268cb"
+    },
+    {
+        id: 2,
+        url: "/img/fastticket_logo.jpeg",
+        text: "Գնել տոմսը FastTicket-ից",
+        link: "https://www.fastticket.am/zhamancayin/e/california-night",
+    },
+    {
+        id: 3,
+        url: "/img/cardPhoto3.webp",
+        text: "Գնել տոմսը Tomsarkgh.am-ից",
+        link: "https://www.tomsarkgh.am/hy/event/49182/"
+    },
+     {
+        id: 4,
+        url: "/PNG/telcell_logo2.png",
+        text: "Գնել Telcell Wallet-ից",
+        link: "https://telcellwallet.page.link/California-night"
+    },
+];
+
+export default data
